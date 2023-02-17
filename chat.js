240efe78035d25948bfb7a641bbe8d811816b14d
Karina <58565453+kbrilhante@users.noreply.github.com>
2023-02-17 14:37:34 -0300

    Add files via upload

--- chat.js
+++ chat.js
@@ -21,7 +21,49 @@ function inicializar() {
     getData();
 }
 
-function getData() {}
+function getData() {
+    const output = document.getElementById("output");
+    firebase.database().ref('/' + nomeSala).on("value", snapshot => {
+        output.innerHTML = "";
+        snapshot.forEach(childSnapshot => {
+            const childKey = childSnapshot.key;
+            if(childKey != "purpose") {
+                const childMsg = childSnapshot.val();
+                const nome = childMsg.nome;
+                const msg = childMsg.mensagem;
+                const likes = childMsg.likes;
+
+                const chatCard = document.createElement("div");
+                chatCard.className = "chatCard";
+                const chatNome = document.createElement("h4");
+                chatNome.className = "chatNome";
+                chatNome.textContent = nome;
+                chatCard.appendChild(chatNome);
+                const row = document.createElement("div");
+                row.className = "row";
+                chatCard.appendChild(row);
+                const col = document.createElement("div");
+                col.className = "col";
+                row.appendChild(col);
+                const chatMsg = document.createElement("h5");
+                chatMsg.className = "chatMsg";
+                chatMsg.textContent = msg;
+                col.appendChild(chatMsg);
+                const colAuto = document.createElement("div");
+                colAuto.className = "col-auto";
+                row.appendChild(colAuto);
+                const botaoLike = document.createElement("button");
+                botaoLike.className = "btn btn-info";
+                botaoLike.id = childKey;
+                botaoLike.value = likes;
+                botaoLike.setAttribute("onclick", "likeMsg(this.id)");
+                botaoLike.innerHTML = '<i class="fa-regular fa-thumbs-up"></i> ' + likes;
+                colAuto.appendChild(botaoLike);
+                output.appendChild(chatCard);
+            }
+        });
+    });
+}
 
 function send() {
     const txtMsg = document.getElementById("msg");
@@ -35,4 +77,12 @@ function send() {
         });
         txtMsg.value = "";
     }
+}
+
+function likeMsg(btnId) {
+    let likes = Number(document.getElementById(btnId).value);
+    likes++;
+    firebase.database().ref('/' + nomeSala).child(btnId).update({
+        likes: likes
+    })
 }
